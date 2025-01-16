794446c2cd8cceb1c2b09b6befb3ad98d42cbc65
Alikhan Sailybaev <sailybaevvv@gmail.com>
2025-01-16 21:36:21 +0500

    ффф

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,20 +1,10 @@
-document.querySelectorAll('.tab').forEach((tab) => {
-  tab.addEventListener('click', () => {
-    // Check if image already exists
-    if (!tab.querySelector('.tab-image')) {
-      const img = document.createElement('img');
-      img.src = '../assets/main2.jpg'; // your image path
-      img.classList.add('tab-image');
-      
-      const line = document.createElement('hr');
-      line.classList.add('tab-line');
-
-      // Append elements
-      tab.appendChild(img);
-      tab.appendChild(line);
-
-      // Trigger a roll-in animation
-      img.style.animation = 'rollIn 0.5s forwards';
-    }
+// Add smooth scrolling behavior
+document.querySelector('.experts-scroll').addEventListener('wheel', (event) => {
+    event.preventDefault();
+    document.querySelector('.experts-scroll').scrollLeft += event.deltaY;
   });
-});
+
+  function scrollToReviews() {
+    const reviewsSection = document.getElementById('reviews');
+    reviewsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
