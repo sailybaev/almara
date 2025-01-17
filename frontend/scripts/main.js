cf728ed1c200c576aff3d5c7d2a3a78ae7432de7
Alikhan Sailybaev <sailybaevvv@gmail.com>
2025-01-17 14:11:04 +0500

    fixed boxings

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,10 +1,40 @@
 // Add smooth scrolling behavior
 document.querySelector('.experts-scroll').addEventListener('wheel', (event) => {
-    event.preventDefault();
-    document.querySelector('.experts-scroll').scrollLeft += event.deltaY;
-  });
+  event.preventDefault();
+  document.querySelector('.experts-scroll').scrollLeft += event.deltaY;
+});
 
-  function scrollToReviews() {
-    const reviewsSection = document.getElementById('reviews');
-    reviewsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+function scrollToMain() {
+  const reviewsSection = document.getElementById('main');
+  reviewsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToAbout() {
+const reviewsSection = document.getElementById('about');
+reviewsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToLore() {
+const reviewsSection = document.getElementById('lore');
+reviewsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToFeatures() {
+const reviewsSection = document.getElementById('features');
+reviewsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToExperts() {
+const reviewsSection = document.getElementById('experts-container');
+reviewsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToReviews() {
+const reviewsSection = document.getElementById('reviews');
+reviewsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function scrollToFooter() {
+const reviewsSection = document.getElementById('footer');
+reviewsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
